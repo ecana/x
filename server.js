8537a363fe45ea61578fd9a326fc4642f1a8a44c
ecana <conluanet@yahoo.com>
2015-01-05 20:17:53 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var http = require('http');
 var ip = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1',
-    port = process.env.OPENSHIFT_NODEJS_PORT || '8080';
+    port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});

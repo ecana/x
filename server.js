aa3310ab90d876083eb58d5feef8e081b00d0353
ecana <conluanet@yahoo.com>
2015-01-05 20:12:27 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 var http = require('http');
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+var ip = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1',
+    port = process.env.OPENSHIFT_NODEJS_PORT || '8080';
 
 http.createServer(function (req, res) {
-    
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('Hello, world!');
-    
-}).listen(server_port);
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+}).listen(port, ip);
+console.log('Server running at http://'+ip+':'+port+'/');
 
 //var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
 //var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
